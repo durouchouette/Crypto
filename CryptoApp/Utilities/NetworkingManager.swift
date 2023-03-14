e6e75bcf898b59077c9295cfcb8ff651d53356e4
Elodie Durouchoux <elodiedurouchoux@gmail.com>
2023-03-14 16:06:25 -0400

    Download coin images with AsyncImage

--- CryptoApp/Utilities/NetworkingManager.swift
+++ CryptoApp/Utilities/NetworkingManager.swift
@@ -10,16 +10,40 @@ import Foundation
 
 class NetworkingManager {
     
-    static func download(url: URL) -> AnyPublisher<Data, any Error>{
-        return URLSession.shared.dataTaskPublisher(for: url) // This URLSession will keep on liscenning so line 46 we cancel the task
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
+    enum NetworkingError: LocalizedError {
+        case badUrlResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badUrlResponse(url: let url): return "Bad response from URL: \(url)"
+            case .unknown: return "Unkown error occured"
             }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, any Error>{
+        return URLSession.shared.dataTaskPublisher(for: url) // This URLSession will keep on listenning so line 46 we cancel the task
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher() // allows us to return a convenient "small" return type
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badUrlResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        print(completion)
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
     }
 }
